abd5c0e32f47bfe43695be1b3a479e1346ef6369
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-07-07 13:57:12 +0200

    Bruker cookie i stedet for auth-header

--- src/main/kotlin/no/nav/navno/api/tokenx/TokenxService.kt
+++ src/main/kotlin/no/nav/navno/api/tokenx/TokenxService.kt
@@ -1,17 +1,19 @@
 package no.nav.navno.api.tokenx
 
-import io.ktor.http.HttpHeaders
 import io.ktor.request.ApplicationRequest
-import io.ktor.request.header
 import no.nav.tms.token.support.tokendings.exchange.TokendingsService
 
-class TokenxService(val tokendingsService: TokendingsService) {
+class TokenxService(val tokendingsService: TokendingsService, val OIDC_COOKIE_NAME: String = "selvbetjening-idtoken") {
+
 
     suspend fun exchangeAuthToken(request: ApplicationRequest, targetApp: String): String {
-        val authorization = request.header(HttpHeaders.Authorization)!!
+        val selvbetjeningIdtoken = request.cookies[OIDC_COOKIE_NAME]!!
         val token =
-            if (authorization.startsWith("Bearer ")) authorization.substring(7, authorization.length)
-            else authorization
+            if (selvbetjeningIdtoken.startsWith("Bearer ")) selvbetjeningIdtoken.substring(
+                7,
+                selvbetjeningIdtoken.length
+            )
+            else selvbetjeningIdtoken
         return tokendingsService.exchangeToken(token, targetApp)
     }
 }
