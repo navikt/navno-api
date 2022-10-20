a986fec61b2b63bbffc06e0ccc037c57a3086377
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-10-20 11:40:52 +0200

    Installert retries på HttpClient

--- src/main/kotlin/no/nav/navno/api/config/HttpClientBuilder.kt
+++ src/main/kotlin/no/nav/navno/api/config/HttpClientBuilder.kt
@@ -2,6 +2,7 @@ package no.nav.navno.api.config
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
+import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
@@ -12,6 +13,9 @@ object HttpClientBuilder {
         return HttpClient(Apache) {
             install(ContentNegotiation) {
                 json(jsonConfig())
+            }
+            install(HttpRequestRetry) {
+                retryOnExceptionOrServerErrors(maxRetries = 5)
             }
             install(HttpTimeout)
             expectSuccess = false
