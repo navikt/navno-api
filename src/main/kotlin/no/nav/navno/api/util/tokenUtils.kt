42f806dc2a3901ec5714972092f8688232a0eeff
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-05-11 14:18:06 +0200

    Fjernet loginservice-validering

--- src/main/kotlin/no/nav/navno/api/util/tokenUtils.kt
+++ src/main/kotlin/no/nav/navno/api/util/tokenUtils.kt
@@ -3,9 +3,7 @@ package no.nav.navno.api.util
 import io.ktor.server.request.ApplicationRequest
 import io.ktor.server.request.authorization
 
-private val OIDC_COOKIE_NAME: String = "selvbetjening-idtoken"
-
 fun getAuthTokenFromRequest(request: ApplicationRequest): String {
-    return request.authorization()?.replace("Bearer ", "") ?: request.cookies[OIDC_COOKIE_NAME]
-    ?: throw RuntimeException("Kunne ikke utlede token fra request")
+    return request.authorization()?.replace("Bearer ", "")
+        ?: throw RuntimeException("Kunne ikke utlede token fra request")
 }
