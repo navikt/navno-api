558e01dbc80a52a873bb4cb9f8411de1193efd73
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-06-07 17:48:33 +0200

    Forenklet gradle-oppsett

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
-    // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
-    kotlin("jvm").version(Kotlin.version)
-    kotlin("plugin.allopen").version(Kotlin.version)
-    kotlin("plugin.serialization").version(Kotlin.version)
+    kotlin("jvm") version("1.8.21")
+    kotlin("plugin.allopen") version("1.8.21")
+    kotlin("plugin.serialization") version("1.8.21")
 
-    id(Shadow.pluginId) version (Shadow.version)
-    id(Versions.pluginId) version Versions.version // ./gradlew dependencyUpdates to check for new versions
+    id("com.github.johnrengelman.shadow") version("8.1.1")
+    id("com.github.ben-manes.versions") version("0.46.0") // ./gradlew dependencyUpdates to check for new versions
     application
 }
 
@@ -21,33 +20,47 @@ repositories {
 }
 
 dependencies {
-    implementation(Auth0.jwt)
-    implementation(Kotlinx.coroutines)
-    implementation(Kotlinx.htmlJvm)
-    implementation(Ktor.clientApache)
-    implementation(Ktor.clientContentNegotiation)
-    implementation(Ktor.clientJson)
-    implementation(Ktor.clientLogging)
-    implementation(Ktor.clientSerialization)
-    implementation(Ktor.htmlBuilder)
-    implementation(Ktor.serverNetty)
-    implementation(Ktor.serverCors)
-    implementation(Ktor.serverDefaultHeaders)
-    implementation(Ktor.serverContentNegotiation)
-    implementation(Ktor.serverAuth)
-    implementation(Ktor.serverCallLogging)
-    implementation(Ktor.serialization)
-    implementation(Ktor.metricsMicrometer)
-    implementation(Micrometer.registryPrometheus)
-    implementation(Logback.classic)
-    implementation(Logstash.logbackEncoder)
-    implementation(NAV.tokenValidatorKtor)
-    implementation(TmsKtorTokenSupport.tokendingsExchange)
-    testImplementation(Mockk.mockk)
-    testImplementation(Ktor.clientMock)
-    testImplementation(Ktor.serverTestHost)
-    testImplementation(Junit.api)
-    testImplementation(Junit.engine)
+    val versions = object {
+        val auth0Jwt = "4.4.0"
+        val kotlinxCoroutines = "1.7.1"
+        val kotlinxHtmlJvm = "0.8.1"
+        val ktor = "2.3.1"
+        val micrometer = "1.11.0"
+        val logback = "1.4.7"
+        val logstash = "7.3"
+        val navSecurity = "3.1.0"
+        val tmsKtorTokenSupport = "2.0.1"
+        val mockk = "1.13.5"
+        val junit = "5.9.2"
+    }
+
+    implementation("com.auth0:java-jwt:${versions.auth0Jwt}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${versions.kotlinxCoroutines}")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:${versions.kotlinxHtmlJvm}")
+    implementation("io.ktor:ktor-client-apache:${versions.ktor}")
+    implementation("io.ktor:ktor-client-content-negotiation:${versions.ktor}")
+    implementation("io.ktor:ktor-client-json:${versions.ktor}")
+    implementation("io.ktor:ktor-client-logging:${versions.ktor}")
+    implementation("io.ktor:ktor-client-serialization:${versions.ktor}")
+    implementation("io.ktor:ktor-server-html-builder:${versions.ktor}")
+    implementation("io.ktor:ktor-server-netty:${versions.ktor}")
+    implementation("io.ktor:ktor-server-cors:${versions.ktor}")
+    implementation("io.ktor:ktor-server-default-headers:${versions.ktor}")
+    implementation("io.ktor:ktor-server-content-negotiation:${versions.ktor}")
+    implementation("io.ktor:ktor-server-auth:${versions.ktor}")
+    implementation("io.ktor:ktor-server-call-logging:${versions.ktor}")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:${versions.ktor}")
+    implementation("io.ktor:ktor-server-metrics-micrometer:${versions.ktor}")
+    implementation("io.micrometer:micrometer-registry-prometheus:${versions.micrometer}")
+    implementation("ch.qos.logback:logback-classic:${versions.logback}")
+    implementation("net.logstash.logback:logstash-logback-encoder:${versions.logstash}")
+    implementation("no.nav.security:token-validation-ktor-v2:${versions.navSecurity}")
+    implementation("com.github.navikt.tms-ktor-token-support:token-support-tokendings-exchange:${versions.tmsKtorTokenSupport}")
+    testImplementation("io.mockk:mockk:${versions.mockk}")
+    testImplementation("io.ktor:ktor-client-mock:${versions.ktor}")
+    testImplementation("io.ktor:ktor-server-test-host:${versions.ktor}")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${versions.junit}")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:${versions.junit}")
 }
 
 application {
@@ -63,5 +76,3 @@ tasks {
         }
     }
 }
-
-apply(plugin = Shadow.pluginId)
