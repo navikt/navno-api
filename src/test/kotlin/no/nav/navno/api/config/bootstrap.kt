d7c11f5b7346a460c5e10b4982ed7dfc56033dc2
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-02-23 10:23:46 +0100

    Fjernet call logging

--- src/test/kotlin/no/nav/navno/api/config/bootstrap.kt
+++ src/test/kotlin/no/nav/navno/api/config/bootstrap.kt
@@ -3,10 +3,7 @@ package no.nav.navno.api.config
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
-import io.ktor.server.plugins.callloging.CallLogging
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.server.request.httpMethod
-import io.ktor.server.request.path
 import io.ktor.server.routing.routing
 import no.nav.navno.api.meldekort.meldekortApi
 
@@ -15,16 +12,6 @@ fun Application.testModule(appContext: TestApplicationContext) {
 
     install(ContentNegotiation) {
         json(jsonConfig())
-    }
-
-    install(CallLogging) {
-        filter { call -> !call.request.path().contains("internal") }
-        format { call ->
-            val status = call.response.status()
-            val httpMethod = call.request.httpMethod.value
-            val path = call.request.path()
-            "$status - $httpMethod $path"
-        }
     }
 
     routing {
