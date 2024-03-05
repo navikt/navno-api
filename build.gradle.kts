c9942bc5d124154a52b835d0dacfbc3ca2d354ad
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-03-05 14:09:35 +0100

    Refactor pga dependabot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,16 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
-    val versions = object {
-        val kotlin = "1.9.22"
-        val shadow = "8.1.1"
-        val versions = "0.51.0"
-    }
+    val kotlinVersion = "1.9.22"
+    val shadowVersion = "8.1.1"
+    val versionsVersion = "0.51.0"
 
-    kotlin("jvm") version(versions.kotlin)
-    kotlin("plugin.allopen") version(versions.kotlin)
-    kotlin("plugin.serialization") version(versions.kotlin)
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.allopen") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
 
-    id("com.github.johnrengelman.shadow") version(versions.shadow)
-    id("com.github.ben-manes.versions") version(versions.versions) // ./gradlew dependencyUpdates to check for new versions
+    id("com.github.johnrengelman.shadow") version shadowVersion
+    id("com.github.ben-manes.versions") version versionsVersion // ./gradlew dependencyUpdates to check for new versions
     application
 }
 
@@ -26,47 +24,45 @@ repositories {
 }
 
 dependencies {
-    val versions = object {
-        val auth0Jwt = "4.4.0"
-        val kotlinxCoroutines = "1.8.0"
-        val kotlinxHtmlJvm = "0.11.0"
-        val ktor = "2.3.8"
-        val micrometer = "1.12.3"
-        val logback = "1.5.0"
-        val logstash = "7.4"
-        val navSecurity = "4.1.3"
-        val tmsKtorTokenSupport = "4.0.0"
-        val mockk = "1.13.9"
-        val junit = "5.10.2"
-    }
+    val auth0JwtVersion = "4.4.0"
+    val kotlinxCoroutinesVersion = "1.8.0"
+    val kotlinxHtmlJvmVersion = "0.11.0"
+    val ktorVersion = "2.3.8"
+    val micrometerVersion = "1.12.3"
+    val logbackVersion = "1.5.0"
+    val logstashVersion = "7.4"
+    val navSecurityVersion = "4.1.3"
+    val tmsKtorTokenSupportVersion = "4.0.0"
+    val mockkVersion = "1.13.9"
+    val junitVersion = "5.10.2"
 
-    implementation("com.auth0:java-jwt:${versions.auth0Jwt}")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${versions.kotlinxCoroutines}")
-    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:${versions.kotlinxHtmlJvm}")
-    implementation("io.ktor:ktor-client-apache:${versions.ktor}")
-    implementation("io.ktor:ktor-client-content-negotiation:${versions.ktor}")
-    implementation("io.ktor:ktor-client-json:${versions.ktor}")
-    implementation("io.ktor:ktor-client-logging:${versions.ktor}")
-    implementation("io.ktor:ktor-client-serialization:${versions.ktor}")
-    implementation("io.ktor:ktor-server-html-builder:${versions.ktor}")
-    implementation("io.ktor:ktor-server-netty:${versions.ktor}")
-    implementation("io.ktor:ktor-server-cors:${versions.ktor}")
-    implementation("io.ktor:ktor-server-default-headers:${versions.ktor}")
-    implementation("io.ktor:ktor-server-content-negotiation:${versions.ktor}")
-    implementation("io.ktor:ktor-server-auth:${versions.ktor}")
-    implementation("io.ktor:ktor-server-call-logging:${versions.ktor}")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:${versions.ktor}")
-    implementation("io.ktor:ktor-server-metrics-micrometer:${versions.ktor}")
-    implementation("io.micrometer:micrometer-registry-prometheus:${versions.micrometer}")
-    implementation("ch.qos.logback:logback-classic:${versions.logback}")
-    implementation("net.logstash.logback:logstash-logback-encoder:${versions.logstash}")
-    implementation("no.nav.security:token-validation-ktor-v2:${versions.navSecurity}")
-    implementation("com.github.navikt.tms-ktor-token-support:tokendings-exchange:${versions.tmsKtorTokenSupport}")
-    testImplementation("io.mockk:mockk:${versions.mockk}")
-    testImplementation("io.ktor:ktor-client-mock:${versions.ktor}")
-    testImplementation("io.ktor:ktor-server-test-host:${versions.ktor}")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:${versions.junit}")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:${versions.junit}")
+    implementation("com.auth0:java-jwt:$auth0JwtVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:$kotlinxHtmlJvmVersion")
+    implementation("io.ktor:ktor-client-apache:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-client-json:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging:$ktorVersion")
+    implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+    implementation("io.ktor:ktor-server-html-builder:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-cors:$ktorVersion")
+    implementation("io.ktor:ktor-server-default-headers:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth:$ktorVersion")
+    implementation("io.ktor:ktor-server-call-logging:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
+    implementation("io.micrometer:micrometer-registry-prometheus:$micrometerVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
+    implementation("no.nav.security:token-validation-ktor-v2:$navSecurityVersion")
+    implementation("com.github.navikt.tms-ktor-token-support:tokendings-exchange:$tmsKtorTokenSupportVersion")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
 
 application {
