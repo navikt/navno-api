7cee6261dc0e52d870c2a4bec00d9c91f4984d1b
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-10-11 13:33:25 +0200

    Refactor tester

--- src/test/kotlin/no/nav/navno/api/config/DummyTokendingsService.kt
+++ src/test/kotlin/no/nav/navno/api/config/DummyTokendingsService.kt
@@ -3,7 +3,5 @@ package no.nav.navno.api.config
 import no.nav.tms.token.support.tokendings.exchange.TokendingsService
 
 class DummyTokendingsService : TokendingsService {
-    override suspend fun exchangeToken(token: String, targetApp: String): String {
-        return ""
-    }
+    override suspend fun exchangeToken(token: String, targetApp: String) = "dummy token"
 }
--- src/test/kotlin/no/nav/navno/api/config/mocks/meldekortMocks.kt
+++ src/test/kotlin/no/nav/navno/api/config/mocks/meldekortMocks.kt
@@ -3,20 +3,22 @@ package no.nav.navno.api.config.mocks
 import io.ktor.client.engine.mock.MockRequestHandleScope
 import io.ktor.client.engine.mock.respond
 import io.ktor.client.engine.mock.respondError
+import io.ktor.client.request.HttpResponseData
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.headersOf
 import io.ktor.http.isSuccess
-import no.nav.navno.api.config.readJson
+import no.nav.navno.api.testutils.readJsonFile
 
 
-fun MockRequestHandleScope.mockMeldekort(status: HttpStatusCode) =
-    if (status.isSuccess()) {
+fun MockRequestHandleScope.mockMeldekort(status: HttpStatusCode): HttpResponseData {
+    return if (status.isSuccess()) {
         respond(
-            readJson("/json/meldekortstatus.json"),
+            content = readJsonFile("/json/meldekortstatus.json"),
             headers = headersOf(HttpHeaders.ContentType, ContentType.Application.Json.toString())
         )
     } else {
         respondError(status)
     }
+}
